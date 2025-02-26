482243b98c89c95b04b75c74427f2ed08745cb36
Siriroj-Srisararkorn <32966637+Siriroj-Srisararkorn@users.noreply.github.com>
2025-02-27 01:37:04 +0700

    Create wl9bet.js

---
+++ wl9bet.js
@@ -0,0 +1,79 @@
+
+//JAVA SCRIPTS ONLY
+var day = "27";
+var month = "2";
+var year = "2025";
+
+var hours = "1";
+var minute = "30";
+var second = "59";
+var milliseconds = "850";
+
+/******************************* */
+
+var date_time_shoot = new Date();
+date_time_shoot.setFullYear(year);
+date_time_shoot.setMonth(month-1);
+date_time_shoot.setDate(day);
+date_time_shoot.setHours(hours);
+date_time_shoot.setMinutes(minute);
+date_time_shoot.setSeconds(second);
+date_time_shoot.setMilliseconds(milliseconds);
+
+/******************************* */
+
+var total_shoot_arr = [];
+total_shoot_arr.push(date_time_shoot);
+Play_Shot(total_shoot_arr);
+
+/******************************* 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
+
+function Play_Shot(shoot_array){
+
+    for(var count = 0; count < shoot_array.length; count++){
+  
+      var shoot_current = new Date(shoot_array[count]);
+  
+      ClickRandomNumberButton();
+      ShotButton(shoot_current);
+  
+    }
+  
+  }
+
+//---------------------[ Random ]---------------------//
+  function ClickRandomNumberButton(){
+    //Click button random number
+    window.document.getElementsByClassName("text-white text-[14px] lg:text-[22px]")[1].click();
+  }
+
+
+//---------------------[ Click ]---------------------//
+   function ShotButton(time_shot){
+
+    var milliseconds_delay = (time_shot - new Date());
+
+    window.setTimeout(function(){
+      window.document.getElementsByClassName("ant-typography font-22")[0].click();
+    }, (time_shot - new Date()));
+
+    var wait_delay = milliseconds_delay / 1000;
+    wait_delay = wait_delay + 5;
+
+
+  }
